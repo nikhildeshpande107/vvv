4a00a7c793422386ad7b68a923f2f952963cda66
nikhildeshpande107 <79147662+nikhildeshpande107@users.noreply.github.com>
2026-03-18 20:18:16 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
+app.post("/users", async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.json(user);
+})
+
+app.get("/users", async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+});
+
+app.delete("/users/:id", async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.send("Deleted");
+});
+
+
+module.exports = mongoose.model("User", userSchema);
+
+const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/authdb")
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
@@ -30,4 +50,13 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model("User", userSchema);
+
+const User = mongoose.model("User", {
+    name: String,
+    email: String
+});
+User.find() 
+
+
+const cors = require("cors");
+app.use(cors());
